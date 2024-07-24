841db799095db2e4385b39bbd1eb199b947b2be6
Yulia Sukhareva <93522379+Suharichek@users.noreply.github.com>
2024-07-24 13:13:40 +0300

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Сухарик on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
